96b30081e21055da7e2a2ed3f07bb8c7001f22f0
Erik Schnetter <schnetter@gmail.com>
2025-12-11 11:20:37 -0500

    CI: Patch MPICH and OpenMPI

--- test/version_f08.f90
+++ test/version_f08.f90
@@ -34,8 +34,9 @@ program version_f08
   print '("MPI implementation:")'
   print '(a)', trim(library_version)
 
-  call MPI_Abi_get_info(abi_info)
   print '("MPI ABI info:")'
+  call MPI_Abi_get_info(abi_info)
+  if (abi_info == MPI_INFO_NULL) stop 1
   call MPI_Info_get_nkeys(abi_info, nkeys)
   do n = 0, nkeys-1
      call MPI_Info_get_nthkey(abi_info, n, key)
@@ -45,45 +46,42 @@ program version_f08
   end do
 
   ! This requires MPI_Init, although it probably shouldn't
-  call MPI_Abi_get_fortran_info(abi_info)
   print '("MPI ABI Fortran info:")'
-  if (abi_info /= MPI_INFO_NULL) then
-     call MPI_Info_get_nkeys(abi_info, nkeys)
-     do n = 0, nkeys-1
-        call MPI_Info_get_nthkey(abi_info, n, key)
-        call MPI_Info_get(abi_info, key, len(value), value, flag)
-        if (.not.flag) stop 1
-        print '("   ",i0,": ",a,"=",a)', n, trim(key), trim(value)
-     end do
-  else
-     print '("   (unavailable)")'
-  end if
+  call MPI_Abi_get_fortran_info(abi_info)
+  if (abi_info == MPI_INFO_NULL) stop 1
+  call MPI_Info_get_nkeys(abi_info, nkeys)
+  do n = 0, nkeys-1
+     call MPI_Info_get_nthkey(abi_info, n, key)
+     call MPI_Info_get(abi_info, key, len(value), value, flag)
+     if (.not.flag) stop 1
+     print '("   ",i0,": ",a,"=",a)', n, trim(key), trim(value)
+  end do
 
   print '("MPI ABI Fortran booleans:")'
-  ! call MPI_Abi_get_fortran_booleans(1, logical1_true, logical1_false, is_set)
-  ! if (is_set) then
-  !    print '("   logical*1: true=",i0,", false=",i0)', logical1_true, logical1_false
-  ! else
-  !    print '("   logical*1: (not set)")'
-  ! end if
-  ! call MPI_Abi_get_fortran_booleans(2, logical2_true, logical2_false, is_set)
-  ! if (is_set) then
-  !    print '("   logical*2: true=",i0,", false=",i0)', logical2_true, logical2_false
-  ! else
-  !    print '("   logical*2: (not set)")'
-  ! end if
+  call MPI_Abi_get_fortran_booleans(1, logical1_true, logical1_false, is_set)
+  if (is_set) then
+     print '("   logical*1: true=",i0,", false=",i0)', logical1_true, logical1_false
+  else
+     print '("   logical*1: (not set)")'
+  end if
+  call MPI_Abi_get_fortran_booleans(2, logical2_true, logical2_false, is_set)
+  if (is_set) then
+     print '("   logical*2: true=",i0,", false=",i0)', logical2_true, logical2_false
+  else
+     print '("   logical*2: (not set)")'
+  end if
   call MPI_Abi_get_fortran_booleans(4, logical4_true, logical4_false, is_set)
   if (is_set) then
      print '("   logical*4: true=",i0,", false=",i0)', logical4_true, logical4_false
   else
      print '("   logical*4: (not set)")'
   end if
-  ! call MPI_Abi_get_fortran_booleans(8, logical8_true, logical8_false, is_set)
-  ! if (is_set) then
-  !    print '("   logical*8: true=",i0,", false=",i0)', logical8_true, logical8_false
-  ! else
-  !    print '("   logical*8: (not set)")'
-  ! end if
+  call MPI_Abi_get_fortran_booleans(8, logical8_true, logical8_false, is_set)
+  if (is_set) then
+     print '("   logical*8: true=",i0,", false=",i0)', logical8_true, logical8_false
+  else
+     print '("   logical*8: (not set)")'
+  end if
   ! call MPI_Abi_get_fortran_booleans(16, logical16_true, logical16_false, is_set)
   ! if (is_set) then
   !    print '("   logical*16: true=",i0,", false=",i0)', logical16_true, logical16_false
