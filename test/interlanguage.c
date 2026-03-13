296d924c1f8f309b730a8b2ef9e1917ffb9c2d33
Erik Schnetter <schnetter@gmail.com>
2026-03-13 12:41:19 -0400

    Move MPI_Comm_f2c and friends to the C MPI library

--- test/interlanguage.c
+++ test/interlanguage.c
@@ -1,19 +1,49 @@
 #include <mpi.h>
-#include <stdio.h>
 
-void subf90_(const MPI_Fint *fcomm, const int *rank, const int *size);
+#include <stdio.h>
+#include <stdlib.h>
+
+void subf90_(const MPI_Fint *fcomm, const int *rank, const int *size, int *fstatus);
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
+
   MPI_Comm comm;
   MPI_Comm_dup(MPI_COMM_WORLD, &comm);
+
   int rank, size;
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &size);
   printf("Interlanguage C: %d/%d\n", rank, size);
+
+  MPI_Status status;
+  int sendbuf, recvbuf;
+  sendbuf = rank;
+  recvbuf = -1;
+  MPI_Sendrecv(&sendbuf, 1, MPI_INT, (rank + 1) % size, 0, &recvbuf, 1, MPI_INT, (rank + size - 1) % size, 0,
+               comm, MPI_STATUS_IGNORE);
+  if (recvbuf != (rank + size - 1) % size)
+    abort();
+
+  sendbuf = rank;
+  recvbuf = -1;
+  MPI_Sendrecv(&sendbuf, 1, MPI_INT, (rank + 1) % size, 0, &recvbuf, 1, MPI_INT, (rank + size - 1) % size, 0,
+               comm, &status);
+  if (status.MPI_SOURCE != (rank + size - 1) % size || status.MPI_TAG != 0)
+    abort();
+  if (recvbuf != (rank + size - 1) % size)
+    abort();
+
   MPI_Fint fcomm = MPI_Comm_c2f(comm);
-  subf90_(&fcomm, &rank, &size);
+  MPI_Fint fstatus[MPI_F_STATUS_SIZE];
+  subf90_(&fcomm, &rank, &size, fstatus);
+  MPI_Status_f2c(fstatus, &status);
+  if (status.MPI_SOURCE != fstatus[MPI_F_SOURCE] || status.MPI_TAG != fstatus[MPI_F_TAG])
+    abort();
+
   MPI_Comm_free(&comm);
+
   MPI_Finalize();
+
   return 0;
 }
--- test/interlanguage_subf90.f90
+++ test/interlanguage_subf90.f90
@@ -1,11 +1,28 @@
-subroutine subf90(comm, crank, csize)
+subroutine subf90(comm, crank, csize, status)
   use mpi
   implicit none
   integer comm, crank, csize
   integer rank, size
+  integer sendbuf, recvbuf
+  integer status(MPI_STATUS_SIZE)
   integer ierr
+
   call MPI_Comm_rank(comm, rank, ierr)
   call MPI_Comm_size(comm, size, ierr)
   print '("Interlanguage Fortran: ",i0,"/",i0)', rank, size
+
   if (rank /= crank .or. size /= csize) stop 1
+
+  sendbuf = rank;
+  recvbuf = -1;
+  call MPI_Sendrecv(sendbuf, 1, MPI_INT, mod(rank + 1, size), 0, recvbuf, 1, MPI_INT, mod(rank + size - 1, size), 0, &
+       comm, MPI_STATUS_IGNORE, ierr)
+  if (recvbuf /= mod(rank + size - 1, size)) stop 1
+
+  sendbuf = rank
+  recvbuf = -1;
+  call MPI_Sendrecv(sendbuf, 1, MPI_INT, mod(rank + 1, size), 0, recvbuf, 1, MPI_INT, mod(rank + size - 1, size), 0, &
+       comm, status, ierr)
+  if (status(MPI_SOURCE) /= mod(rank + size - 1, size) .or. status(MPI_TAG) /= 0) stop
+  if (recvbuf /= mod(rank + size - 1, size)) stop 1
 end subroutine subf90
