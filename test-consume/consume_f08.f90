ca7beb77fab5a7b71f9a133df43b154b18c78208
Erik Schnetter <schnetter@gmail.com>
2026-05-09 13:44:38 -0400

    Add test for using this package via pkg-config

---
+++ test-consume/consume_f08.f90
@@ -0,0 +1,25 @@
+program consume_f08
+  use mpi_f08
+  implicit none
+
+  logical :: initialized, finalized
+
+  call MPI_Initialized(initialized)
+  if (initialized) stop 1
+  call MPI_Finalized(finalized)
+  if (finalized) stop 1
+
+  call MPI_Init()
+
+  call MPI_Initialized(initialized)
+  if (.not.initialized) stop 1
+  call MPI_Finalized(finalized)
+  if (finalized) stop 1
+
+  call MPI_Finalize()
+
+  call MPI_Initialized(initialized)
+  call MPI_Finalized(finalized)
+  if (.not.finalized) stop 1
+
+end program consume_f08
